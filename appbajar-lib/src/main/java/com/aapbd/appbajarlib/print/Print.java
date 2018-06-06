77b39f3c7a9e39c265fbe63313b970f769659028
Biplob <javaorjava@gmail.com>
2018-06-06 16:40:51 +0600

    Updated

--- appbajar-lib/src/main/java/com/aapbd/appbajarlib/print/Print.java
+++ appbajar-lib/src/main/java/com/aapbd/appbajarlib/print/Print.java
@@ -4,7 +4,54 @@ import android.util.Log;
 
 public class Print {
 
+
+     private static Print instance;
+
+     private boolean shouldPrint=true;
+
+     public static Print getInstance ()
+     {
+         if(instance==null)
+             instance=new Print();
+
+         return instance;
+     }
+
+    private Print() {
+    }
+
+
+    public boolean isShouldPrint() {
+        return shouldPrint;
+    }
+
+    public void setShouldPrint(boolean shouldPrint) {
+        this.shouldPrint = shouldPrint;
+    }
+
+    public void printError(String message) {
+        printError(null, message);
+    }
+
+    public  void printError(String TAG, String message)
+    {
+
+        if(!shouldPrint)
+            return;
+
+        if(TAG==null)
+        {
+
+            Log.e("Error :", message + "");
+        }else
+        {
+            Log.e(TAG + "", message + "");
+        }
+
+    }
+
     public static void message(String message) {
+
 
         Log.e("Message is:", message + "");
 
